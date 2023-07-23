e030ddd390985aa6b280cdbb48e1cd7817161d33
jiushi21 <zc931325671@gmail.com>
2023-07-24 07:28:24 +0800

    number ast重构代码，测试成功

--- parser.ts
+++ parser.ts
@@ -5,21 +5,39 @@ export enum NodeTypes {
   Number,
 }
 
+interface Node {
+  type: NodeTypes;
+}
+
+interface RootNode extends Node {
+  body: Node[];
+}
+interface NumberNode extends Node {
+  value: string;
+}
+
+function createRootNode(): RootNode {
+  return {
+    type: NodeTypes.Root,
+    body: [],
+  };
+}
+
+function createNumberNode(value: string): NumberNode {
+  return {
+    type: NodeTypes.Number,
+    value: "2",
+  };
+}
+
 export function parser(tokens: Token[]) {
   let current = 0;
   let token = tokens[current];
 
-  const rootNode: any = {
-    type: NodeTypes.Root,
-    body: [],
-  };
+  const rootNode = createRootNode();
 
   if (token.type === TokenTypes.Number) {
-    const numberNode = {
-      type: NodeTypes.Number,
-      value: "2",
-    };
-    rootNode.body.push(numberNode);
+    rootNode.body.push(createNumberNode(token.value));
   }
   return rootNode;
 }
